72eb0765f1a1e1f89e4ac5d7fff70a6f31d12a32
brooklynn mcdade <brooklynn.mcdade@gmail.com>
2020-10-06 20:08:00 -0500

    SPA

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// importing all as a Module object
-import * as components from "./components";
-// importing all by name
+import Navigo from "navigo";
+import { capitalize } from "lodash";
 import { Header, Nav, Main, Footer } from "./components";
-
+import * as state from "./store";
+const router = new Navigo(window.location.origin);
 function render(st = state.Home) {
-  document.querySelector(“#root”).innerHTML = `
+  document.querySelector("#root").innerHTML = `
   ${Header(st)}
   ${Nav(state.Links)}
   ${Main(st)}
   ${Footer()}
 `;
+  router.updatePageLinks();
+}
+render(state.Home);
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
