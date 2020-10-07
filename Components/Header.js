79054f175998fb782be9bb7acf883784aa674f17
brooklynn mcdade <brooklynn.mcdade@gmail.com>
2020-10-06 20:38:02 -0500

    logo

--- Components/Header.js
+++ Components/Header.js
@@ -1,5 +1,5 @@
 export default () => `
 <header>
-  <img class="logo" src="Pictures/MNJpeg.jpg">
+  <img class="logo" src="https://github.com/BrooklynnM/CapstoneMN/blob/master/Pictures/MNJpeg.jpg?raw=true">
 </header>
 `;
